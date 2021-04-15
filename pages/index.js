584adb8c0eca5448b59bb7afcf766ba61e177dd3
thallyssonklein <thallyssonklein@gmail.com>
2021-04-14 23:24:53 -0300

    Video done

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,30 @@ import EmailContextProvider from "./EmailContext";
 
 export default function Home() {
   return (
-    <div
-      style={{
-        position: "absolute",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
-        textAlign: "center",
-      }}
-    >
-      <h1 style={{ fontFamily: "arial" }}>
-        Imagine that your Notion collections create a life of their own and start to organize themselves, what do you think?
-      </h1>
-      <EmailContextProvider><TextInput/></EmailContextProvider>
+    <div style={{position : "absolute", top : 0, left : 0, right : 0, bottom : 0, margin : 0, padding : 0, backgroundColor: "#2c3334"  }}>
+        <div
+          style={{
+            display : 'flex',
+            position : 'relative',
+            flex : 1,
+            alignItems : 'center',
+            justifyContent : 'center',
+            flexDirection : 'column',
+            padding : 0,
+            margin : 0,
+            paddingTop : 50,
+            textAlign: "center",
+            backgroundColor: "#2c3334"
+          }}
+        >
+          <h1 style={{ fontFamily: "arial", color : "#d3d6d7" }}>
+            Imagine that your Notion collections create a life of their own and start to organize themselves, what do you think?
+          </h1>
+          <video src={require('./video.mp4')} autoPlay loop/>   
+          <div style={{marginTop : 50}}>
+            <EmailContextProvider><TextInput/></EmailContextProvider>
+          </div>
+        </div>
     </div>
   );
 }
