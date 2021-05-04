648297d641cee6207fc45f20278310f773b03703
Ricardo Passos <ricardo.passos.cr@gmail.com>
2021-05-04 10:16:25 -0300

    The index page was not working because "firebase/app" had not been imported on that pape.

--- components/TextInput.js
+++ components/TextInput.js
@@ -3,6 +3,7 @@ import {
     FirestoreProvider,
     FirestoreMutation,
 } from "@react-firebase/firestore";
+import firebse from "firebase/app";
 import { EmailContext } from "../contexts/EmailContext";
 import SubmitButton from "./SubmitButton";
 import config from "../utils/firestoreConfig";
