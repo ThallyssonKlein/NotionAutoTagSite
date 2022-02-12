e2dea901f881e04f1a3fd79f393709ad6e5a6885
thallyssonklein <thallyssonklein@gmail.com>
2022-02-12 15:21:54 -0300

    error 500 fixed

--- components/app/main/CallToAction/index.jsx
+++ components/app/main/CallToAction/index.jsx
@@ -8,7 +8,7 @@ export default function ConnectWithNotion() {
     parameters = `client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&response_type=code`;
   }
 
-  alert(parameters);
+  console.log(parameters);
 
   return (
     <>
